339909cb177cef74f6b205ecca4181a18933acdb
nehoehne <nehoehne@gmail.com>
2024-09-26 16:57:44 -0500

    Gereral page loading spinner

---
+++ src/lib/Spinner.svelte
@@ -0,0 +1,10 @@
+<script>
+const 	size = 12
+  const color = 'black'
+</script>
+
+<div class={`fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-55`}>
+	<div 
+	  class={`w-${size} h-${size} border-4 border-${color} rounded-full border-t-transparent animate-spin`}
+	></div>
+  </div>
