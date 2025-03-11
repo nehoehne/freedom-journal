99cd785775de95fd8b72d53ef8414f715af93bdf
Kalen Arendt <dev.KalenArendt@Gmail.com>
2025-03-10 19:51:36 -0500

    Close the Journal Entry Dialog after saving.

--- src/lib/JournalEntry.svelte
+++ src/lib/JournalEntry.svelte
@@ -11,17 +11,18 @@
 		yellowActivities as allYellowActivities,
 		redActivities as allRedActivities,
 		addEntry,
-		updateEntry
+		updateEntry,
 	} from "../stores/store";
 
 	export let type: JournalEntryType;
 	export let entry: Entry;
+	export let open: boolean = true; // used to set the value in the parent to close the dialog this component is contained within
 
 	// If state was provided then we want to populate the form
 	let date: DateValue | undefined = entry.getDateAsDateValue();
 	let text: string = entry.getText();
 
-	const handleSubmit = () => {
+	const save = () => {
 		if (entry) {
 			entry.setDate(date);
 			entry.setText(text);
@@ -32,56 +33,56 @@
 				} else if (type == JournalEntryType.NEW) {
 					addEntry(entry);
 				}
+
+				/* Close the Dialog */
+				open = false;
 			} else {
 				console.log("Missing date.");
 			}
-		} else console.log("Missing state.");
+		} else {
+			console.log("Missing state.");
+		}
 	};
 
 	const isReadonly = () => type == JournalEntryType.READONLY;
 </script>
 
 <div>
-	<form on:submit|preventDefault={handleSubmit}>
-		<DatePicker
-			bind:date={date}
-			disabled={type == JournalEntryType.READONLY}
-		/>
-		<Textarea
-			bind:value={text}
-			placeholder="How was your day?"
-			class="mt-3 mb-3"
-			disabled={isReadonly()}
-		/>
+	<DatePicker bind:date disabled={type == JournalEntryType.READONLY} />
+	<Textarea
+		bind:value={text}
+		placeholder="How was your day?"
+		class="mt-3 mb-3"
+		disabled={isReadonly()}
+	/>
 
-		<!-- Green Activities -->
-		<ActivityList
-			selectedActivitiesMap={entry.getGreenActivitiesMap()}
-			allActivities={$allGreenActivities}
-			disabled={isReadonly()}
-		></ActivityList>
-		<hr class="mt-3 mb-3" />
-		
-		<!-- Yellow Activities -->
-		<ActivityList
-			selectedActivitiesMap={entry.getYellowActivitiesMap()}
-			allActivities={$allYellowActivities}
-			disabled={isReadonly()}
-		></ActivityList>
-		<hr class="mt-3 mb-3" />
+	<!-- Green Activities -->
+	<ActivityList
+		selectedActivitiesMap={entry.getGreenActivitiesMap()}
+		allActivities={$allGreenActivities}
+		disabled={isReadonly()}
+	></ActivityList>
+	<hr class="mt-3 mb-3" />
 
-		<!-- Red Activities -->
-		<ActivityList
-			selectedActivitiesMap={entry.getRedActivitiesMap()}
-			allActivities={$allRedActivities}
-			disabled={isReadonly()}
-		></ActivityList>
+	<!-- Yellow Activities -->
+	<ActivityList
+		selectedActivitiesMap={entry.getYellowActivitiesMap()}
+		allActivities={$allYellowActivities}
+		disabled={isReadonly()}
+	></ActivityList>
+	<hr class="mt-3 mb-3" />
 
-		<!-- Only show Save button if editable -->
-		{#if type == JournalEntryType.NEW || type == JournalEntryType.EDIT}
-			<div class="mt-6 float-end">
-				<Button variant="outline" type="submit">Save</Button>
-			</div>
-		{/if}
-	</form>
+	<!-- Red Activities -->
+	<ActivityList
+		selectedActivitiesMap={entry.getRedActivitiesMap()}
+		allActivities={$allRedActivities}
+		disabled={isReadonly()}
+	></ActivityList>
+
+	<!-- Only show Save button if editable -->
+	{#if type == JournalEntryType.NEW || type == JournalEntryType.EDIT}
+		<div class="mt-6 float-end">
+			<Button variant="outline" on:click={() => save()}>Save</Button>
+		</div>
+	{/if}
 </div>
