5933c2ec17ea29c273d455d8ecd5f285471eabe6
NEHoehne <NEHoehne@gmail.com>
2025-06-20 18:44:08 -0500

    asdf

--- src/lib/StyledCard.svelte
+++ src/lib/StyledCard.svelte
@@ -1,3 +1,3 @@
-<div class="max-w-md mx-auto m-4 px-6 py-4 flex flex-col items-center gap-4 bg-gradient-to-br from-gray-100 via-white to-gray-100 rounded-2xl shadow-lg">
+<div class="max-w-md mx-auto m-4 px-6 py-4 flex flex-col items-center gap-4 bg-gradient-to-br rounded-2xl shadow-lg">
     <slot/>
 </div>
